f01667609c630baaeaf389a22a09d1022a57bd65
jvega48 <jvega4849@gmail.com>
2018-11-21 09:28:53 -0800

    Fixed contract and added unit testing

--- token_sale/contracts/DappToken.sol
+++ token_sale/contracts/DappToken.sol
@@ -2,9 +2,19 @@ pragma solidity ^0.4.2;
 
 contract DappToken {
 
+    // name 
+    // symbol
+    
+    string public name = "Dapp Token";
+    string public symbol = "Dapp";
+    string public standard = "Dapp Token v1.0";
     uint256 public totalSupply;
+    
+    mapping(address => uint256) public balanceOf;
 
-    function DappToken () public {
-        totalSupply = 100000;
+    function DappToken (uint256 _initialSupply) public {
+        // account msg.sender 
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
     }
 }
--- token_sale/test/DappToken.js
+++ token_sale/test/DappToken.js
@@ -1,12 +1,31 @@
 const DappToken = artifacts.require('../contracts/DappToken.sol')
 
 contract('DappToken', (accounts) =>{
+    let tokenInstance;
+    it('set the total suply upon deployment', () =>{
+        return DappToken.deployed().then( (instance) =>{
+            tokenInstance = instance;
+            return tokenInstance.name()
+        }).then( (name) => {
+            assert.equal(name, 'Dapp Token', 'working name')
+            return tokenInstance.symbol();
+        }).then( (symbol)=> {
+            assert.equal(symbol, 'Dapp', 'working symbol')
+            return tokenInstance.standard();
+        }).then( (standard)=> {
+            assert.equal(standard, 'Dapp Token v1.0', 'standard symbol')
+        })
+    })
+
     it('set the total suply upon deployment', () =>{
         return DappToken.deployed().then((instance) =>{
             tokenInstance = instance
             return tokenInstance.totalSupply()
         }).then( (totalSupply) =>{
             assert.equal(totalSupply.toNumber(), 100000, 'set total supply to 100000')
+            return tokenInstance.balanceOf(accounts[0]);
+        }).then((adminBalance) =>{
+            assert.equal(adminBalance.toNumber(), 100000, 'it allocated the initial balance to the admin')
         })
     })
 })
